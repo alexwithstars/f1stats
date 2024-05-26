6f0a6767f037e42c5c3a0e7515959c153a3c776f
alexwithstars <rayo080378@gmail.com>
2024-05-26 14:25:31 -0600

    fix(404): move from element 'a' to 'Link' from react router

--- src/404.jsx
+++ src/404.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './404.css'
 
 export function NotFound () {
@@ -5,7 +6,7 @@ export function NotFound () {
     <div className='not-found'>
       <h1>404</h1>
       <p>Página no encontrada</p>
-      <a href='/'>Volver al inicio</a>
+      <Link to='/'>Volver al inicio</Link>
     </div>
   )
 }
