1a91f24d1017a40de83c24df8deb560d0cea722b
alexwithstars <rayo080378@gmail.com>
2025-05-08 21:20:41 -0600

    fix(DriverCard): update image URL to use a better scale

--- src/components/DriverCard.jsx
+++ src/components/DriverCard.jsx
@@ -2,11 +2,12 @@ import { getFlagCdn } from '../services/f1api'
 import './DriverCard.css'
 
 export function DriverCard ({ number, imgUrl, name, country, team, color }) {
+  const refinedImgUrl = imgUrl.replace(/1col/, '2col')
   return (
     <article className='driver' style={{ '--back': `#${color}` }}>
       <section className='driver-presentation'>
         <span className='driver-team-back'>{team}</span>
-        <img src={imgUrl} className='driver-img' />
+        <img src={refinedImgUrl} className='driver-img' />
         <span className='driver-team-front'>{team}</span>
       </section>
       <section className='driver-info'>
