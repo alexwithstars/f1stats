85fad38733dd2f74404fa76414254bce625640a8
alexwithstars <rayo080378@gmail.com>
2025-07-10 13:27:38 -0600

    Renombrar extensiones .js/.jsx a .ts/.tsx

--- src/misc/constants.js
+++
@@ -1,7 +0,0 @@
-export const routes = {
-  '/races': 'Carreras',
-  '/drivers': 'Pilotos'
-}
-
-export const fallbackFlagUrl = '/fallbackFlag.svg'
-export const fallbackAvatarUrl = '/fallbackAvatar.svg'
